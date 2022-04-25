0a9b567b31148b09525b5896375b2f1b93f49eb2
Vika <TomchukVS@mpei.ru>
2022-04-25 15:45:25 +0300

    добавление новых тестов

--- svg.h
+++ svg.h
@@ -6,17 +6,14 @@ using namespace std;
 
 void svg_begin(double width, double height);
 
-void svg_text(double left, double baseline, string text);
-
-
-void svg_rect (double x, double y, double width, double height, string stroke = "black", string fill = "black");
-
-
 void svg_end();
 
+size_t check_size(size_t text_size);
+
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
+
+void svg_text(double left, double baseline, string text, size_t size = 12);
+
 void show_histogram_svg(const vector<size_t>& bins);
-
-
-
 
 #endif // SVG_H_INCLUDED
