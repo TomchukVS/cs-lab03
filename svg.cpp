e37d084685995e6c56d173f229d1ff967de8248a
Vika <TomchukVS@mpei.ru>
2022-04-25 15:44:43 +0300

    добавление новых тестов

--- svg.cpp
+++ svg.cpp
@@ -13,9 +13,9 @@ void svg_begin(double width, double height)
 }
 
 void
-svg_text(double left, double baseline, string text)
+svg_text(double left, double baseline, string text, size_t size)
 {
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+   cout << "<text x='" << left << "'y='" << baseline << "'font-size='"<< size << "'>" << text <<"</text>";
 }
 
 void svg_rect (double x, double y, double width, double height, string stroke , string fill )
@@ -28,6 +28,14 @@ svg_end()
 {
     cout << "</svg>\n";
 }
+size_t check_size(size_t text_size){
+    if (text_size < 8 || text_size > 32){
+        return(0);
+    }
+    else{
+        return(text_size);
+    }
+}
 void
 show_histogram_svg(const vector<size_t>& bins)
 
@@ -39,32 +47,40 @@ show_histogram_svg(const vector<size_t>& bins)
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
-    double top = 0;
-    svg_begin(IMAGE_WIDTH,IMAGE_HEIGHT);
-
-    const auto MAX_BIN_WIDTH = IMAGE_WIDTH - TEXT_WIDTH;
-    size_t max_bins_width = bins[0];
-    for (size_t bin : bins)
-    {
-        if (bin > max_bins_width)
-        {
-            max_bins_width = bin;
+    const auto MAX_ASTERISK = IMAGE_WIDTH - TEXT_WIDTH;
+    size_t text_size = 0;
+    while(text_size == 0){
+        cerr << "Enter text size [8-32] : ";
+        cin >> text_size;
+        text_size = check_size(text_size);
+        if (text_size == 0){
+            char s;
+            cerr << "Text size out of bounds, do you whant to enter new text size? [y/n]";
+            cin >> s;
+            if (s == 'n'){
+                text_size = 12;
+            }
         }
     }
-    max_bins_width = max_bins_width * BLOCK_WIDTH;
-
-    for (size_t bin : bins)
-    {
-        double bin_width = BLOCK_WIDTH * bin;
-        if (max_bins_width >= MAX_BIN_WIDTH)
-        {
-            bin_width = MAX_BIN_WIDTH * (bin_width / max_bins_width) - 1;
+   size_t max_count = 0;
+    for (size_t bin : bins) {
+        if (bin * BLOCK_WIDTH > max_count) {
+            max_count = bin * BLOCK_WIDTH;
         }
-
-
-    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
-    svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
-     top += BIN_HEIGHT;
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+        size_t bin_scale = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            bin_scale = (size_t)(bin * scaling_factor);
+        }
+        const double bin_width = BLOCK_WIDTH * bin_scale;
+         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin), text_size);
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        top += BIN_HEIGHT;
     }
     svg_end();
 }
