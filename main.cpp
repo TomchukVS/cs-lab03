9593c121c33eb2b0de52c525f946a2a7973b4192
Vika <TomchukVS@mpei.ru>
2022-05-28 19:11:11 +0300

    сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -3,108 +3,96 @@
 #include "histogram.h"
 #include "svg.h"
 #include <curl/curl.h>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-
-vector<double>
-input_numbers(istream& in, size_t count)
+vector<double> input_numbers(istream& in, size_t count)
 {
-    vector<double> result(count);
-    for(size_t i = 0; i < count; i++)
-    {
-        in >> result[i];
-    }
-    return result;
+vector<double> result(count);
+for(size_t i = 0; i < count; i++)
+{
+in >> result[i];
 }
+return result;
+}
+
 Input read_input(istream& in, bool prompt)
 {
-    Input data;
-    size_t number_count;
-    if (prompt)
-    {
-        cerr << "Enter number count: ";
-    }
-    in >> number_count;
+Input data;
 
-    if(prompt)
-    {
-        cerr << "Enter numbers: ";
-    }
-    data.numbers = input_numbers(in, number_count);
+size_t number_count;
+if (prompt)
+{
+cerr << "Enter number count: ";
+}
+in >> number_count;
 
-    if(prompt)
-    {
-        cerr << "Enter bin count: ";
-    }
-    in >> data.bin_count;
+if(prompt)
+{
+cerr << "Enter numbers: ";
+}
+data.numbers = input_numbers(in, number_count);
 
-    return data;
+if(prompt)
+{
+cerr << "Enter bin count: ";
+}
+in >> data.bin_count;
+
+return data;
 }
 
-
-
-void show_histogramm_text(const vector<size_t>& bins)
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx)
 {
-    size_t SCREEN_WIDTH = 80;
-    const size_t max_asterix = SCREEN_WIDTH - 3 - 1;
-
-    size_t max_bin = bins[0];
-    for (size_t bin: bins)
-    {
-        if (max_bin < bin)
-        {
-            max_bin = bin;
-        }
-    }
-
-    for(size_t bin: bins)
-    {
-        size_t height = bin;
-
-//Проверка, нужно ли масштабировать столбцы
-        if (max_bin > max_asterix)
-        {
-            height = max_asterix * (static_cast<double>(bin) / max_bin);
-        }
-
-        if (bin < 100)
-        {
-            cout << ' ';
-        }
-        if (bin < 10)
-        {
-            cout << ' ';
-        }
-        cout << bin << "|";
-        for (size_t i = 0; i < height; i++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+// TODO: дописывать данные к буферу.
+size_t data_size=item_size*item_count;
+stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+buffer->write(reinterpret_cast<const char*>(items), data_size);
+return data_size;
 }
 
-
-
-int main(int argc, char* argv[])
+Input
+download(const string& address)
 {
-    if(argc > 1)
-    {
-        curl_global_init(CURL_GLOBAL_ALL);
-        CURL *curl = curl_easy_init();
-        if(curl)
-        {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-            if (res)
-            {
-                cerr << curl_easy_strerror(res) << endl;
-                exit(1);
-            }
-        }
-        return 0;
-    }
+stringstream buffer;
+
+// TODO: заполнить буфер.
+curl_global_init(CURL_GLOBAL_ALL);
+CURL *curl = curl_easy_init();
+if(curl)
+{
+CURLcode res;
+curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+res = curl_easy_perform(curl);
+if (res)
+{
+cerr << curl_easy_strerror(res) << endl;
+exit(1);
+}
+}
+curl_easy_cleanup(curl);
+return read_input(buffer, false);
+
+}
+
+int
+main(int argc, char* argv[])
+{
+Input input;
+if (argc > 1)
+{
+input = download(argv[1]);
+}
+else
+{
+input = read_input(cin, true);
+}
+
+const auto bins = make_histogramm(input);
+show_histogramm_svg(bins);
 }
