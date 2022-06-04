f15d9b50abca3d8c2e3c8923eeda98619ca54268
Vika <TomchukVS@mpei.ru>
2022-06-04 13:04:47 +0300

    lab04-1

--- histogram.h
+++ histogram.h
@@ -6,6 +6,13 @@
 using namespace std;
 
 void find_minmax(const vector<double>& numbers, double& min, double& max);
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
+vector<size_t> make_histogramm(Input data);
 
 
+void show_histogram_text(const vector<size_t>& bins);
 #endif // HISTOGRAM_H_INCLUDED
