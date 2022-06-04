7bbd04072647da71b1ccd26b573bd582d11afbd7
Vika <TomchukVS@mpei.ru>
2022-06-04 14:33:10 +0300

    форматный вывод в стиле С

--- main.cpp
+++ main.cpp
@@ -1,120 +1,101 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
 #include "histogram.h"
 #include "svg.h"
+#include <curl/curl.h>
+#include <sstream>
+#include <string>
+
 using namespace std;
 
-
-vector <double> input_numbers ( size_t count)
+vector<double> input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
-    for (size_t i = 0; i < count; i++)
+    for(size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
 
-
-
-vector <size_t> make_histogram( const vector<double>& numbers, size_t bin_count)
+Input read_input(istream& in, bool prompt)
 {
-    double min, max;
-    find_minmax ( numbers, min, max) ;
-    vector<size_t> bins(bin_count,0); //переменная показывающая количество чисел в заданном диапазоне
-    double bin_size =( max-min)/ bin_count; //разме корзины
-    for(size_t i=0; i < numbers.size(); i++)
-    {
-        bool found=false;
-        for ( size_t j = 0; j <( bin_count-1) && !found; j++)
-        {
-            auto lo = min + j * bin_size; // нижняя граница корзины
-            auto hi = min + (j+1) * bin_size; // верхняя граница корзины
-            if((lo <= numbers[i]) && (numbers[i] < hi))
-            {
-                bins[j]++;
-                found = true;
-            }
-        }
-        if (!found) //для максимального числа
-        {
-            bins [bin_count-1]++;
+    Input data;
 
-        }
-    }
-    return bins;
-}
-
-show_histogram_text(const vector <size_t>& bins)
-{
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH -3-1;
-
-    size_t max_bin = bins[0]; //максимальная высота столбца
-    for (size_t bin: bins)
-    {
-        if(max_bin < bin)
-        {
-            max_bin = bin;
-        }
-    }
-    for(size_t bin: bins)
-    {
-        size_t height = bin;
-//проверить, нужно ли масштабировать данные, если нужно, пересчитать height
-        if (max_bin > MAX_ASTERISK)
-        {
-            height = MAX_ASTERISK*(static_cast<double> (bin) / max_bin); //коэфициент
-        }
-        size_t procent= bin;
-        if (procent < 100)
-        {
-            cout << " ";
-        }
-        if (procent < 10)
-        {
-            cout << " ";
-        }
-        cout << procent << "|";
-        for (size_t i=0; i < height; i++)
-        {
-            cout<< "*";
-        }
-        cout << endl;
-    }
-}
-
-
-
-
-
-int main()
-{
-//Ввод данных
     size_t number_count;
-    cerr<< "Enter number count: ";
-    cin>> number_count;
-    if(number_count == 0)
+    if (prompt)
     {
-        cerr << "There is nothing to compute\n";
-        return 0;
+        cerr << "Enter number count: ";
+    }
+    in >> number_count;
+
+    if(prompt)
+    {
+        cerr << "Enter numbers: ";
+    }
+    data.numbers = input_numbers(in, number_count);
+
+    if(prompt)
+    {
+        cerr << "Enter bin count: ";
+    }
+    in >> data.bin_count;
+
+    return data;
+}
+
+
+
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx)
+{
+    size_t data_size=item_size*item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
+}
+
+Input
+download(const string& address)
+{
+    stringstream buffer;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *curl = curl_easy_init();
+    if(curl)
+    {
+        char *ip;
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        if ((res == CURLE_OK) && !curl_easy_getinfo(curl, CURLINFO_PRIMARY_IP, &ip) && ip) cerr << "IP:" << ip << endl;
+        else
+        {
+            cout << curl_easy_strerror(res) << endl;
+            exit(1);
+        }
+    }
+    curl_easy_cleanup(curl);
+    return read_input(buffer, false);
+
+}
+
+int
+main(int argc, char* argv[])
+{
+    Input input;
+    if (argc > 1)
+    {
+        input = download(argv[1]);
+    }
+    else
+    {
+        input = read_input(cin, true);
     }
 
-
-    cerr<< "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
-
-    size_t bin_count;
-    cerr <<"Enter bin count: ";
-    cin >> bin_count;
-
-
-//Расчет гистограммы
-    const auto bins = make_histogram (numbers, bin_count);
-
-//Вывод гистограммы
+    const auto bins = make_histogramm(input);
     show_histogramm_svg(bins);
-
-    return 0;
 }
